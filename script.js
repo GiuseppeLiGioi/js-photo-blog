bad6ea78b9eacd161ff218cbb258035eb996fbc2
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-30 15:31:10 +0100

    ADD: overlay (html-css)

--- script.js
+++ script.js
@@ -12,6 +12,34 @@ Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come po
 
 Milestone 3
 Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+
+
+
+
+
+
+//GIORNO 2
+
+Milestone 1
+Facciamo in modo di creare un overlay che copra l’intera pagina e all’interno, centrata, disponiamo un’immagine qualunque ed un button di chiusura.
+
+
+Milestone 2
+Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
+Dopodiché facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
+Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
+
+
+
+Milestone 3
+Inseriamo il pezzo di logica finale: quando una foto viene cliccata, dobbiamo fare in modo che sia proprio quella foto a essere mostrata all’interno dell’overlay.
+Ci sono diversi modi di farlo, prova a sperimentare :faccia_leggermente_sorridente:
+
+
+Bonus
+Spostandosi col mouse sopra le foto, queste si zoommano, ruotano di 10 gradi e la loro ombra aumenta, il tutto in manierà fluida. 
+Inoltre il mouse diventa un puntatore, per far capire all’utente che può cliccare
+Buon Lavoro
 */
 
 
